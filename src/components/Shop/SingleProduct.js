c39ebc403a0ac712a146c01b17a0e1421fbe55a0
degun <pretty.lonely.place@hotmail.com>
2020-11-05 17:11:04 +0100

    single

--- src/components/Shop/SingleProduct.js
+++ src/components/Shop/SingleProduct.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Container from '../_common/Container';
+import Button from '../_common/Button';
 import { withRouter } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { useDispatch } from 'react-redux';
@@ -17,25 +18,42 @@ function SingleProduct({match}){
     const { data: recommendationsData } = useQuery(GET_PRODUCT_RECOMMENDATIONS, {variables: {productId}});
     const images = imgs?.edges?.map(({node}) => node.transformedSrc) ?? [];
 
-    console.log(variants);
-
     useEffect(() => {
         dispatch(setMenuOpen(false));
         setSelectedImage(0)
     }, [handle])
 
+    const variantsShown = variants?.edges?.filter(({node}) => node.title !== "Default Title") ?? [];
+    const thisVariant = variants?.edges[selectedVariant] ?? {};
+
+    console.log(thisVariant)
+
     return(
         <Container>
             <div className="Producto">
                 <div className="images">
-                    <div className="images-list">{images.map((src, i) => <img onClick={() => setSelectedImage(i)} src={src} alt="" className={selectedImage === i ? "selected" : ""} key={`product-image-${i}`} />)}</div>
-                    <img className="image" src={images[selectedImage]} onMouseMove={e => console.log(e)} alt="" />
+                    <div className="images-list">
+                        {images.map((src, i) => <img onClick={() => {
+                            setSelectedImage(i);
+                            if(i > (images.length - variantsShown.length - 1)){
+                                setSelectedVariant(i - (images.length - variantsShown.length))
+                            }
+                        }} src={src} alt="" className={selectedImage === i ? "selected" : ""} key={`product-image-${i}`} />)}
+                    </div>
+                    <img className="image" src={images[selectedImage]} alt="" />
                 </div>
                 <div className="information">
                     <h1>{title}</h1>
-                    <div className="variants">
-                        <h4>Variante: </h4>{variants?.edges?.map(({node}, i) => <div onClick={() => setSelectedVariant(i)} className={`variant ${selectedVariant === i ? "selected" : ""}`}>{node.title}</div>) ?? null}
-                    </div>     
+                    {variantsShown.length ? <div className="variants">
+                        <h4>Variante: </h4>{variantsShown.map(({node}, i) => <div key={node.title} onClick={() => {setSelectedVariant(i);setSelectedImage(i + (images.length - variantsShown.length))}} className={`variant ${selectedVariant === i ? "selected" : ""}`}>{node.title}</div>)}
+                    </div> : null} 
+                    <div className="buy">
+                        <div className="buy-prices">
+                            <span className="price">{new Intl.NumberFormat('sq-AL', { style: 'currency', currency: 'ALL' }).format(0)}</span>
+                            {0 !== "0.0" ? <span className="compare">{new Intl.NumberFormat('sq-AL', { style: 'currency', currency: 'ALL' }).format(0)}</span> : null}
+                        </div>
+                        <Button variant="primary">Shto në Shportë</Button>
+                    </div> 
                 </div>
             </div>
         </Container>
